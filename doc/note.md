c9dc72cf93251454416092227b345201e1c5cb8b
zsytssk <zsytssk@gmail.com>
2022-03-08 10:37:48 +0800

    master:[update]

--- doc/note.md
+++ doc/note.md
@@ -1,3 +1,9 @@
 ## 2022-03-07 09:45:56
 
 - @ques sass css import className 有没有用
+
+  - 没用
+
+- @ques 还有哪些最新的技术？
+  - wasm
+  - rust
--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,22 @@
-import { useState } from "react";
-import logo from "./logo.svg";
-import "./App.css";
-
-import style1 from "./test1.module.scss";
-import style2 from "./test2.module.scss";
+import { useEffect, useState } from 'react';
+import logo from './logo.svg';
+import './App.less';
 
 function App() {
-  const [count, setCount] = useState(0);
+    const [count, setCount] = useState(0);
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p className={style1.wrap}>
-          this is a test for module.scss
-          <button
-            className={style1.btn}
-            onClick={() => setCount((count) => count + 1)}
-          >
-            count is: {count}
-          </button>
-          <button
-            className={style2.btn}
-            onClick={() => setCount((count) => count + 1)}
-          >
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {" | "}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  );
+    useEffect(() => {
+        console.log(count);
+    }, [count]);
+
+    return (
+        <div className="App">
+            <div className="wrap">
+                <img src={logo} className="inner" alt="logo" />
+                <button onClick={() => setCount(count + 1)}>{count}</button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
